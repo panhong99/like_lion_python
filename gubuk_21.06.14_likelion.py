5780bf5facc52f6a322553c6ee99fc8f5c6977e4
Lim pan hong <klng12345@naver.com>
2021-06-15 17:10:15 +0900

    21.06.15_likelion

---
+++ gubuk_21.06.14_likelion.py
@@ -0,0 +1,119 @@
+# '''turtle모듈로 학습'''
+# import turtle as t
+# t.shape("turtle") # shape안에 다른 단어를 넣으면 다른 문양이 나온다.
+
+# '''pixel = 이미지를 이루는 가장 최소의 단위
+# 픽셀 수치가 높을 수록 화면을 더 조밀하게(자세하게) 구성할 수 있다.'''
+
+#t.pencolor("BLUE")
+#t.fillcolor("RED")
+#t.begin_fill()
+#t.fd(100)
+#t.rt(90)
+#t.fd(100)
+#t.rt(90)
+#t.fd(100)
+#t.rt(90)
+#t.fd(100)
+#.end_fill()
+
+#'''완료'''
+#''' for문을 이용하여 오각형 만들기 '''
+#'''input을 활용하여 다각형 그리기'''
+
+#n = 5
+#for i in range(n):
+#    t.forward(100)
+#    t.right((360/n)*2)
+#    t.forward(100)
+#   t.left(360/n)
+    
+    #t.forward(100)
+    #t.right(72)
+    #t.speed("fastest")
+    
+
+    
+#t.pencolor("#dcdcdc") 
+#t.forward(100)
+#t.right(90)
+#.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90)
+
+#t.up()
+#t.goto(200,0)
+#t.down()
+
+#t.pencolor("#ff0000")
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90 + 90)
+
+#t.up()
+#t.goto(300 , - 200)
+#t.down()
+
+#t.pencolor("#0000ff")
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90 + 90)
+
+#t.up()
+#t.goto(100 ,-300)
+#t.down()
+#t.pensize(5)
+#t.pencolor("#00ff00")
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#.right(90)
+
+##삼각형 만들기
+#t.penup()
+#t.goto(-200 , -50)
+#t.pendown()
+#t.circle(40 , steps = 3)
+
+#사각형 만들기
+#t.penup()
+#t.goto(-100 , -50)
+#t.pendown()
+#t.circle(40 , steps = 4)
+
+##오각형 만들기
+#t.penup()
+#t.goto(0 , 50)
+#t.pendown()
+#.circle(40 , steps = 5)
+
+##육각형 만들기
+#t.penup()
+#t.goto(100 , -50)
+#t.pendown()
+#t.circle(40 , steps = 6)
+
+## 원 만들기
+#t.penup()
+#t.goto(200 , -50)
+#t.pendown()
+#t.circle(40)
+
+
